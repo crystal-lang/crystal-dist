a3f0ec65f41b56ee893079d5fedee28b9a1a826e
Brian J. Cardiff <bcardiff@gmail.com>
2020-01-14 09:44:33 -0300

    Remove explicit sudo in setup.sh
    Otherwise it fail when doing 
    
    $ curl -sSL https://dist.crystal-lang.org/apt/setup.sh | sudo bash

--- dist/apt/setup.sh
+++ dist/apt/setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-curl -L https://keybase.io/crystal/pgp_keys.asc | sudo apt-key add -
+curl -L https://keybase.io/crystal/pgp_keys.asc | apt-key add -
 echo "deb https://dist.crystal-lang.org/apt crystal main" > /etc/apt/sources.list.d/crystal.list
 apt-get update
